702a8c4f42c14d1f7825c18300ca0e740c23dd30
TAUFIK DARUL IKHROM <taufikdi27@gmail.com>
2026-03-06 11:11:01 +0700

    fix : memperbaiki error function doGet

--- src/Kode.js
+++ src/Kode.js
@@ -1,3 +1,9 @@
+function doGet() {
+    return HtmlService.createHtmlOutputFromFile('Index')
+        .setTitle('The Ultimate Blueprint 3.0')
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
 function include(filename) {
     return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
